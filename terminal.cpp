70748b3aa70b583efc37021b28689e85a2d064d7
airtooz <ericchen0802@gmail.com>
2016-09-23 15:22:10 +0800

    Create terminal.cpp

---
+++ terminal.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include "terminal.h"
+
+using namespace std;
+
+
+void Terminal::setterminal_name(string name)
+{
+	terminal_name = name;
+}
+void Terminal::setterminal_x(int x)
+{
+	terminal_x = x;
+}
+void Terminal::setterminal_y(int y)
+{
+	terminal_y = y;
+}
+string Terminal::getterminal_name(void)
+{
+	return terminal_name;
+}
+int Terminal::getterminal_x(void)
+{
+	return terminal_x;
+}
+int Terminal::getterminal_y(void)
+{
+	return terminal_y;
+}
