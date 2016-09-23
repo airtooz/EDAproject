64cf60705dd656d6e1ab098b26fc23a479a259fe
airtooz <ericchen0802@gmail.com>
2016-09-23 15:47:49 +0800

    Update contour.cpp

--- contour.cpp
+++ contour.cpp
@@ -10,35 +10,27 @@ Contour::Contour()
 	next = NULL;
 	prev = NULL;
 }
+
 void Contour::setnext(Contour* ptr)
-{
 	next = ptr;
-}
+
 void Contour::setprev(Contour* ptr)
-{
 	prev = ptr;
-}
+
 Contour* Contour::getnext()
-{
 	return next;
-}
+
 Contour* Contour::getprev()
-{
 	return prev;
-}
+
 void Contour::setcontour_x(int x)
-{
 	contour_x = x;
-}
+
 void Contour::setcontour_y(int y)
-{
 	contour_y = y;
-}
+
 int Contour::getcontour_x()
-{
 	return contour_x;
-}
+
 int Contour::getcontour_y()
-{
 	return contour_y;
-}
