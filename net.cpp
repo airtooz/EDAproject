5ed3a64114e0f249c2b20953e56aa89425315750
airtooz <ericchen0802@gmail.com>
2016-09-23 15:59:26 +0800

    Update net.cpp

--- net.cpp
+++ net.cpp
@@ -6,18 +6,13 @@
 using namespace std;
 
 void Net::setdegree(int d)
-{
 	net_degree = d;
-}
+
 void Net::setconnect(vector <string> connect)
-{
 	net_connect = connect;
-}
+
 int Net::getdegree(void)
-{
 	return net_degree;
-}
+
 vector <string> Net::getconnect(void)
-{
 	return net_connect;
-}
