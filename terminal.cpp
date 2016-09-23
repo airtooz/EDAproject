4a6c71b7a1ff197cc599f942ec329fcbcff4924c
airtooz <ericchen0802@gmail.com>
2016-09-23 16:00:33 +0800

    Update terminal.cpp

--- terminal.cpp
+++ terminal.cpp
@@ -4,28 +4,20 @@
 
 using namespace std;
 
-
 void Terminal::setterminal_name(string name)
-{
 	terminal_name = name;
-}
+
 void Terminal::setterminal_x(int x)
-{
 	terminal_x = x;
-}
+
 void Terminal::setterminal_y(int y)
-{
 	terminal_y = y;
-}
+
 string Terminal::getterminal_name(void)
-{
 	return terminal_name;
-}
+
 int Terminal::getterminal_x(void)
-{
 	return terminal_x;
-}
+
 int Terminal::getterminal_y(void)
-{
 	return terminal_y;
-}
